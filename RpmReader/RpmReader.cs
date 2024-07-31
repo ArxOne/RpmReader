7903dae486f70d4bb64e6aaaa47c57a65fafb2e7
pascal <pascal@craponne.fr>
2024-07-31 23:14:13 +0200

    Added v6 RPM tags + logging duplicates + keeping headers in properties

--- RpmReader/RpmReader.cs
+++ RpmReader/RpmReader.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.IO.Compression;
+using System.Text;
+using System.Xml;
 
 namespace ArxOne.Rpm;
 
@@ -9,10 +12,40 @@ public class RpmReader
     private readonly Stream _inputStream;
     private readonly Deserializer _deserializer;
 
+    public IReadOnlyDictionary<RpmTag, object?> Signature { get; private set; }
+    public IReadOnlyDictionary<RpmTag, object?> Header { get; private set; }
+
     public RpmReader(Stream inputStream)
     {
         _inputStream = inputStream;
         _deserializer = new Deserializer();
+        CheckDuplicateTags();
+    }
+
+    private static bool _duplicatesChecked;
+
+    [Conditional("DEBUG")]
+    private static void CheckDuplicateTags()
+    {
+        if (_duplicatesChecked)
+            return;
+
+        _duplicatesChecked = true;
+
+        var namesByValue = new Dictionary<int, ICollection<string>>();
+        foreach (var name in Enum.GetNames<RpmTag>())
+        {
+            var value = (int)Enum.Parse<RpmTag>(name);
+            if (!namesByValue.TryGetValue(value, out var names))
+                namesByValue[value] = names = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            names.Add(name);
+        }
+
+        foreach (var (value, names) in namesByValue)
+        {
+            if (names.Count > 1)
+                Console.WriteLine($"Duplicate tag names for value {value}: {string.Join(',', names)}");
+        }
     }
 
     public IReadOnlyDictionary<RpmTag, object?> Read()
@@ -21,9 +54,9 @@ public class RpmReader
         if (!lead.IsValid)
             throw new InvalidDataException();
 
-        var signature = ReadHeaderIndexes();
-        var info = ReadHeaderIndexes();
-        return info;
+        Signature = ReadHeaderIndexes();
+        Header = ReadHeaderIndexes();
+        return Header;
     }
 
     private IReadOnlyDictionary<RpmTag, object?> ReadHeaderIndexes()
--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -1,7 +1,10 @@
 ﻿
 using ArxOne.Rpm;
 
-using var rpmStream = File.OpenRead($@"C:\Users\pascal\Desktop\dotnet-runtime-8.0-8.0.7-1.x86_64.rpm");
+//using var rpmStream = File.OpenRead(@"C:\Users\pascal\Desktop\dotnet-runtime-8.0-8.0.7-1.x86_64.rpm");
+using var rpmStream = File.OpenRead(@"C:\Users\pascal\Desktop\arxone-backup-10.2.18731.1544-1.noarch.rpm");
 var rpmReader = new RpmReader(rpmStream);
 
 var tags = rpmReader.Read();
+var signature = rpmReader.Signature;
+
